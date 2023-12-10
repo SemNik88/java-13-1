637e2b7c6258bce44b8e565c0c9f12ff016d1907
SemNik <142649558+SemNik88@users.noreply.github.com>
2023-12-10 17:02:40 +0300

    Update ShopRepository.java

--- src/main/java/org/example/ShopRepository.java
+++ src/main/java/org/example/ShopRepository.java
@@ -4,55 +4,51 @@ public class ShopRepository {
     private Product[] products = new Product[0];
 
     /**
-     * Вспомогательный метод для имитации добавления элемента в массив
+     * Метод для добавления товара в репозиторий
      *
-     * @param current — массив, в который мы хотим добавить элемент
-     * @param product — элемент, который мы хотим добавить
-     * @return — возвращает новый массив, который выглядит, как тот, что мы передали,
-     * но с добавлением нового элемента в конец
-     */
-    private Product[] addToArray(Product[] current, Product product) {
-        Product[] tmp = new Product[current.length + 1];
-        for (int i = 0; i < current.length; i++) {
-            tmp[i] = current[i];
-        }
-        tmp[tmp.length - 1] = product;
-        return tmp;
-    }
-
-    /**
-     * Метод добавления товара в репозиторий
-     *
-     * @param product — добавляемый товар
+     * @param product добавляемый товар
      */
     public void add(Product product) {
         products = addToArray(products, product);
     }
 
-    public Product[] findAll() {
-        return products;
-    }
-
-    // Этот способ мы рассматривали в теории в теме про композицию
-    public void remove(int id) {
-        Product productFound = findById(id);
-        if (productFound == null) {
-            throw new NotFoundException(
-                    "Element with id: " + id + " not found"
-            );
+    /**
+     * Метод для удаления товара из репозитория по его ID
+     *
+     * @param id ID удаляемого товара
+     */
+    public void removeById(int id) {
+        Product productToRemove = findById(id);
+        if (productToRemove == null) {
+            throw new NotFoundException("Element with id: " + id + " not found");
         }
-        Product[] tmp = new Product[products.length - 1];
-        int copyToIndex = 0;
-        for (Product product : products) {
-            if (product.getId() != id) {
-                tmp[copyToIndex] = product;
-                copyToIndex++;
+
+        int indexToRemove = -1;
+        for (int i = 0; i < products.length; i++) {
+            if (products[i].getId() == id) {
+                indexToRemove = i;
+                break;
             }
         }
-        products = tmp;
+
+        if (indexToRemove != -1) {
+            Product[] tmp = new Product[products.length - 1];
+            int copyToIndex = 0;
+            for (int i = 0; i < products.length; i++) {
+                if (i != indexToRemove) {
+                    tmp[copyToIndex++] = products[i];
+                }
+            }
+            products = tmp;
+        }
     }
 
-
+    /**
+     * Метод для поиска товара по его ID
+     *
+     * @param id ID искомого товара
+     * @return найденный товар или null, если не найден
+     */
     public Product findById(int id) {
         for (Product product : products) {
             if (product.getId() == id) {
@@ -62,29 +58,29 @@ public class ShopRepository {
         return null;
     }
 
+    /**
+     * Метод для получения всех товаров в репозитории
+     *
+     * @return массив всех товаров в репозитории
+     */
+    public Product[] findAll() {
+        return products;
+    }
 
-    public void removeById(int id) {
-        int indexToRemove = -1;
-        for (int i = 0; i < products.length; i++) {
-            if (products[i].getId() == id) {
-                indexToRemove = i;
-                break;
-            }
-        }
-        if (indexToRemove == -1) {
-            throw new NotFoundException("Element with id: " + id + " not found");
-        }
-
-        Product[] tmp = new Product[products.length - 1];
-        int copyToIndex = 0;
-        for (int i = 0; i < products.length; i++) {
-            if (i != indexToRemove) {
-                tmp[copyToIndex] = products[i];
-                copyToIndex++;
-            }
-        }
-        products = tmp;
+    /**
+     * Вспомогательный метод для добавления элемента в массив
+     *
+     * @param current массив, в который добавляется элемент
+     * @param product добавляемый элемент
+     * @return новый массив с добавленным элементом
+     */
+    private Product[] addToArray(Product[] current, Product product) {
+        Product[] tmp = new Product[current.length + 1];
+        System.arraycopy(current, 0, tmp, 0, current.length);
+        tmp[tmp.length - 1] = product;
+        return tmp;
     }
 }
+
 
 
